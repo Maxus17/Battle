fa0edb78b3aa86f8b49643c220cc876e08f27e5c
Maxus17 <maxusb1@ya.ru>
2024-02-19 22:25:17 +0300

    first step

---
+++ Assets/Scripts/CameraRotations.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotations : MonoBehaviour
+{
+    public Transform CameraAxisTransform;
+    public float RotationSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localEulerAngles = new Vector3(0, transform.localEulerAngles.y + Time.deltaTime*RotationSpeed* Input.GetAxis("Mouse X"), 0);
+        var Value = CameraAxisTransform.localEulerAngles.x - Time.deltaTime * RotationSpeed * Input.GetAxis("Mouse Y");
+        if(Value > 180)
+        {
+            Value -= 360;
+        }
+        Value = Mathf.Clamp(Value, -5, 10);
+        CameraAxisTransform.localEulerAngles = new Vector3(Value, 0, 0);
+;    }
+}
