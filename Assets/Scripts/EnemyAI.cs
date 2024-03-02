9422fae65d20af2f2b47ac12e29444420b2f731c
Maxus17 <maxusb1@ya.ru>
2024-03-03 00:59:44 +0300

    Second step

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    public List<Transform> potrolPoints;
+    private NavMeshAgent _navMeshAgent;
+    public PlayerContoller player;
+    private bool _isPlayerNoticed;
+    public float viewAngle;
+
+    void Start()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();        
+    }
+
+    void Update()
+    {
+        PickPotrolPoint();
+        NoticePlayerUpdate();
+        ChaseUpdate();
+    }
+    private void ChaseUpdate()
+    {
+        if (_isPlayerNoticed)
+        {
+            _navMeshAgent.destination = player.transform.position;
+        }
+    }
+    private void NoticePlayerUpdate()
+    {
+        var direction = player.transform.position - transform.position;
+        _isPlayerNoticed = false;
+
+        if (Vector3.Angle(transform.forward, direction) < viewAngle)
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position + Vector3.up, direction, out hit))
+            {
+                if (hit.collider.gameObject == player.gameObject)
+                {
+                    _isPlayerNoticed = true;
+                }
+            }
+        }
+    }
+    private void PickPotrolPoint()
+    {
+        if (_navMeshAgent.remainingDistance == 0)
+        {
+            _navMeshAgent.destination = potrolPoints[Random.Range(0, potrolPoints.Count)].position;
+        }
+        if(!_isPlayerNoticed)
+        {
+            _navMeshAgent.destination = potrolPoints[Random.Range(0, potrolPoints.Count)].position;
+        }
+    }
+}
